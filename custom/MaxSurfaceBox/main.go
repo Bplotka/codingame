e429b86d72fe181d4b061323976ca35da49f29c8
Bartek Plotka <bwplotka@gmail.com>
2017-01-08 18:34:11 +0000

    Attempt 3 Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- custom/MaxSurfaceBox/main.go
+++ custom/MaxSurfaceBox/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 )
 
 func main() {
 	var bricks int
 	fmt.Scan(&bricks)
+	fmt.Printf("%d\n", cuboidSurface(6, 97, 1697))
 	fmt.Printf("%d %d\n",
 		cuboidSurface(lmin(buildNormalizedCuboid(bricks))),
 		cuboidSurface(lmax(buildTallCuboid(bricks))),
@@ -28,44 +30,31 @@ func buildNormalizedCuboid(bricksNum int) (int, int, int) {
 		return int(rounded), int(rounded), int(rounded)
 	}
 
-	lowerBricksNum := bricksNum - 1
-	rounded := 1
-	// Find the biggest cubic root that can be fitted in searched cubic space.
-	for {
-		var ok bool
-		rounded, ok = tolerantIsInt(math.Pow(float64(lowerBricksNum), 1.0/3.0))
-		if ok {
-			break
-		}
+	var components []int
+	smallestDivisible := 2
+	number := bricksNum
+	for number > 1 {
+		smallestDivisible = findSmallestDivisible(number, smallestDivisible)
+		components = append(components, smallestDivisible)
 
-		lowerBricksNum--
-		continue
+		number = number / smallestDivisible
 	}
 
-	cuboid := []int{rounded, rounded, rounded}
-	cuboid[0] = findSmallestDivisible(bricksNum, cuboid[0])
-	excludedDims := map[int]struct{}{
-		0: struct{}{},
+	sort.Sort(sort.Reverse(sort.IntSlice(components)))
+
+	cuboid := []int{components[0],1,1}
+	for i, comp := range components[1:2] {
+		cuboid[i+1] = comp
 	}
-	for {
-		if cuboidSpace(cuboid) == bricksNum {
-			break
-		}
 
-		if cuboidSpace(cuboid) < bricksNum {
-			dimID := minDim(cuboid, excludedDims)
-			cuboid[dimID] = findSmallestDivisible(bricksNum, cuboid[dimID])
+	if len(components) <= 3 {
+		return cuboid[0], cuboid[1], cuboid[2]
+	}
 
-			if len(excludedDims) >= 2 {
-				return buildTallCuboid(bricksNum)
-			}
-
-			excludedDims[dimID] = struct{}{}
-			continue
-		}
-
-		dimID := maxDim(cuboid, excludedDims)
-		cuboid[dimID] = findBiggestDivisible(bricksNum, cuboid[dimID])
+	sort.Sort(sort.IntSlice(components))
+	for _, comp := range components[:len(components)-2] {
+		minDimID := minDim(cuboid, map[int]struct{}{})
+		cuboid[minDimID] *= comp
 	}
 
 	return cuboid[0], cuboid[1], cuboid[2]
