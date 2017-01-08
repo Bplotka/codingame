2f215247b07486bb67c5b41e6c04b6a6687fe240
Bartek Plotka <bwplotka@gmail.com>
2017-01-08 18:10:54 +0000

    Attempt2
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- custom/MaxSurfaceBox/main.go
+++ custom/MaxSurfaceBox/main.go
@@ -9,9 +9,8 @@ import (
 func main() {
 	var bricks int
 	fmt.Scan(&bricks)
-
 	fmt.Printf("%d %d\n",
-		cuboidSurface(lmin(buildNormalizedCuboid2(bricks))),
+		cuboidSurface(lmin(buildNormalizedCuboid(bricks))),
 		cuboidSurface(lmax(buildTallCuboid(bricks))),
 	)
 }
@@ -28,79 +27,108 @@ func buildNormalizedCuboid(bricksNum int) (int, int, int) {
 		// It can be a cube!
 		return int(rounded), int(rounded), int(rounded)
 	}
+
 	lowerBricksNum := bricksNum - 1
-	for lowerBricksNum > 0 {
-		rounded, ok := tolerantIsInt(math.Pow(float64(lowerBricksNum), 1.0/3.0))
-		if !ok {
-			lowerBricksNum--
-			continue
+	rounded := 1
+	// Find the biggest cubic root that can be fitted in searched cubic space.
+	for {
+		var ok bool
+		rounded, ok = tolerantIsInt(math.Pow(float64(lowerBricksNum), 1.0/3.0))
+		if ok {
+			break
 		}
 
-		rest := bricksNum - lowerBricksNum
-		// Try to build wall/ walls on top of found cubic. We can end up with something like
-		// rounded x rounded x (rounded + some layer).
-		if rest == (rounded * rounded) {
-			return rounded, rounded, rounded + 1
-		}
-
-		restExcludingOneLayer := rest - (rounded * rounded)
-		if restExcludingOneLayer < 0 {
-			// Not enough bricks even for single layer. Damn it.
-			lowerBricksNum--
-			continue
-		}
-
-		// One layer and something! Try to now build up on different dimension and have
-		// rounded x (rounded + some layer) x (rounded + some layer).
-		if restExcludingOneLayer != (rounded * (rounded + 1)) {
-			lowerBricksNum--
-			continue
-		}
-
-		// Nice!
-		return rounded, rounded + 1, rounded + 1
+		lowerBricksNum--
+		continue
 	}
 
-	// fallback to tall cuboid.
-	return buildTallCuboid(bricksNum)
+	cuboid := []int{rounded, rounded, rounded}
+	cuboid[0] = findSmallestDivisible(bricksNum, cuboid[0])
+	excludedDims := map[int]struct{}{
+		0: struct{}{},
+	}
+	for {
+		if cuboidSpace(cuboid) == bricksNum {
+			break
+		}
+
+		if cuboidSpace(cuboid) < bricksNum {
+			dimID := minDim(cuboid, excludedDims)
+			cuboid[dimID] = findSmallestDivisible(bricksNum, cuboid[dimID])
+
+			if len(excludedDims) >= 2 {
+				return buildTallCuboid(bricksNum)
+			}
+
+			excludedDims[dimID] = struct{}{}
+			continue
+		}
+
+		dimID := maxDim(cuboid, excludedDims)
+		cuboid[dimID] = findBiggestDivisible(bricksNum, cuboid[dimID])
+	}
+
+	return cuboid[0], cuboid[1], cuboid[2]
 }
 
-// Minimum (experienced that by doing manual experiments with small number of bricks).
-func buildNormalizedCuboid2(bricksNum int) (int, int, int) {
-	// Try cubic root first.
-	if rounded, ok := tolerantIsInt(math.Pow(float64(bricksNum), 1.0/3.0)); ok {
-		// It can be a cube!
-		return int(rounded), int(rounded), int(rounded)
+func findSmallestDivisible(number int, greaterThan int) int {
+	divisible := greaterThan + 1
+	for divisible < number {
+		if math.Mod(float64(number), float64(divisible)) != 0 {
+			divisible++
+			continue
+		}
+		return divisible
 	}
 
-	// ok then let's start will tall one and try to pack it down.
-	x, y, z := buildTallCuboid(bricksNum)
-	for {
-		// Pack x in y dir.
-		rounded, ok := tolerantIsInt(float64(x) / 2.)
-		if !ok {
-			break
-		}
-		x = rounded
-		y = 2 * y
+	return number
+}
 
-		if x <= z || x <= y {
-			break
+func findBiggestDivisible(number int, smallerThan int) int {
+	divisible := smallerThan - 1
+	for divisible > 1 {
+		if math.Mod(float64(number), float64(divisible)) != 0 {
+			divisible--
+			continue
+		}
+		return divisible
+	}
+
+	return 1
+}
+
+func minDim(cuboid []int, excludingDims map[int]struct{}) int {
+	min := math.MaxInt32
+	minIndex := -1
+	for i, value := range cuboid {
+		if _, ok := excludingDims[i]; ok {
+			continue
 		}
 
-		// Pack x in z dir.
-		rounded, ok = tolerantIsInt(float64(x) / 2.)
-		if !ok {
-			break
-		}
-		x = rounded
-		z = 2 * z
-
-		if x <= z || x <= y {
-			break
+		if value < min {
+			minIndex = i
+			min = value
 		}
 	}
-	return x, y, z
+
+	return minIndex
+}
+
+func maxDim(cuboid []int, excludingDims map[int]struct{}) int {
+	max := 0
+	maxIndex := -1
+	for i, value := range cuboid {
+		if _, ok := excludingDims[i]; ok {
+			continue
+		}
+
+		if value > max {
+			maxIndex = i
+			max = value
+		}
+	}
+
+	return maxIndex
 }
 
 func tolerantIsInt(number float64) (int, bool) {
@@ -124,4 +152,8 @@ func lmax(x, y, z int) (int, int, int) {
 
 func cuboidSurface(x, y, z int) int {
 	return (2 * x * y) + (2 * y * z) + (2 * x * z)
+}
+
+func cuboidSpace(cuboid []int) int {
+	return cuboid[0] * cuboid[1] * cuboid[2]
 }
